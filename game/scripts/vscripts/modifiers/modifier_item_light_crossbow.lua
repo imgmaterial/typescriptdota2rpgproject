82db257cf46c0249ab75016518371ed29e80c20e
_imgmaterial <paul.oshchepkov@icloud.com>
2022-02-23 21:28:35 +0100

    light crossbow modifier added

--- game/scripts/vscripts/modifiers/modifier_item_light_crossbow.lua
+++ game/scripts/vscripts/modifiers/modifier_item_light_crossbow.lua
@@ -1,6 +1,6 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1,["6"] = 1,["7"] = 1,["8"] = 3,["9"] = 4,["10"] = 3,["11"] = 4,["12"] = 4,["14"] = 3,["16"] = 4});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1,["6"] = 1,["7"] = 1,["8"] = 3,["9"] = 4,["10"] = 3,["11"] = 4,["12"] = 11,["13"] = 12,["14"] = 11,["15"] = 15,["16"] = 16,["17"] = 15,["18"] = 19,["19"] = 20,["20"] = 21,["21"] = 23,["22"] = 24,["23"] = 25,["24"] = 26,["26"] = 19,["27"] = 30,["28"] = 31,["29"] = 30,["30"] = 34,["31"] = 36,["32"] = 34,["33"] = 39,["34"] = 41,["35"] = 39,["36"] = 44,["37"] = 46,["38"] = 44,["39"] = 49,["40"] = 51,["41"] = 52,["42"] = 53,["43"] = 56,["44"] = 57,["45"] = 58,["47"] = 62,["49"] = 49,["50"] = 4,["52"] = 3,["54"] = 4});
 local ____exports = {}
 local ____dota_ts_adapter = require("lib.dota_ts_adapter")
 local BaseModifier = ____dota_ts_adapter.BaseModifier
@@ -9,6 +9,44 @@ ____exports.modifier_item_light_crossbow = __TS__Class()
 local modifier_item_light_crossbow = ____exports.modifier_item_light_crossbow
 modifier_item_light_crossbow.name = "modifier_item_light_crossbow"
 __TS__ClassExtends(modifier_item_light_crossbow, BaseModifier)
+function modifier_item_light_crossbow.prototype.IsHidden(self)
+    return true
+end
+function modifier_item_light_crossbow.prototype.IsPurgable(self)
+    return false
+end
+function modifier_item_light_crossbow.prototype.OnCreated(self, params)
+    local ability = self:GetAbility()
+    if ability then
+        self.bonus_agility = ability:GetSpecialValueFor("bonus_agility")
+        self.bonus_damage = ability:GetSpecialValueFor("bonus_damage")
+        self.bonus_attack_speed = ability:GetSpecialValueFor("bonus_attack_speed")
+        self.bonus_distance_damage = ability:GetSpecialValueFor("bonus_distance_damage")
+    end
+end
+function modifier_item_light_crossbow.prototype.DeclareFunctions(self)
+    return {MODIFIER_PROPERTY_STATS_AGILITY_BONUS, MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE, MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT, MODIFIER_PROPERTY_PROCATTACK_BONUS_DAMAGE_PURE}
+end
+function modifier_item_light_crossbow.prototype.GetModifierBonusStats_Agility(self)
+    return self.bonus_agility or 0
+end
+function modifier_item_light_crossbow.prototype.GetModifierPreAttack_BonusDamage(self)
+    return self.bonus_damage or 0
+end
+function modifier_item_light_crossbow.prototype.GetModifierAttackSpeedBonus_Constant(self)
+    return self.bonus_attack_speed or 0
+end
+function modifier_item_light_crossbow.prototype.GetModifierProcAttack_BonusDamage_Pure(self)
+    local attack_target = self:GetParent():GetAttackTarget()
+    local owner_unit = self:GetParent()
+    if attack_target then
+        local damage_per_unit_distance = self.bonus_distance_damage or 0
+        SendToConsole("target")
+        return (owner_unit:GetRangeToUnit(attack_target) * damage_per_unit_distance) / 100
+    else
+        return 0
+    end
+end
 modifier_item_light_crossbow = __TS__Decorate(
     {
         registerModifier(nil)
--- src/vscripts/modifiers/modifier_item_light_crossbow.ts
+++ src/vscripts/modifiers/modifier_item_light_crossbow.ts
@@ -3,6 +3,63 @@ import { BaseModifier, registerModifier } from "../lib/dota_ts_adapter";
 @registerModifier()
 export class modifier_item_light_crossbow extends BaseModifier
 {
+    bonus_agility ?:number;
+    bonus_damage ?:number;
+    bonus_attack_speed ?:number;
+    bonus_distance_damage ?:number;
 
+    IsHidden(): boolean {
+        return true
+    }
 
+    IsPurgable(): boolean {
+        return false
+    }
+
+    OnCreated(params: object): void {
+        const ability = this.GetAbility()
+        if (ability)
+        {
+            this.bonus_agility = ability.GetSpecialValueFor("bonus_agility");
+            this.bonus_damage = ability.GetSpecialValueFor("bonus_damage");
+            this.bonus_attack_speed = ability.GetSpecialValueFor("bonus_attack_speed")
+            this.bonus_distance_damage = ability.GetSpecialValueFor("bonus_distance_damage")
+        }
+    }
+
+    DeclareFunctions() {
+        return [ModifierFunction.STATS_AGILITY_BONUS, ModifierFunction.PREATTACK_BONUS_DAMAGE, ModifierFunction.ATTACKSPEED_BONUS_CONSTANT, ModifierFunction.PROCATTACK_BONUS_DAMAGE_PURE];
+    }
+
+    GetModifierBonusStats_Agility()
+    {
+        return this.bonus_agility ?? 0;
+    }
+
+    GetModifierPreAttack_BonusDamage()
+    {
+        return this.bonus_damage ?? 0;
+    }
+
+    GetModifierAttackSpeedBonus_Constant()
+    {
+        return this.bonus_attack_speed ?? 0;
+    }
+
+    GetModifierProcAttack_BonusDamage_Pure()
+    {
+        const attack_target = this.GetParent().GetAttackTarget()
+        const owner_unit = this.GetParent()
+        if (attack_target)
+        { 
+            
+            const damage_per_unit_distance:number = this.bonus_distance_damage ?? 0;
+            SendToConsole("target")
+            return (owner_unit.GetRangeToUnit(attack_target)*damage_per_unit_distance)/100
+        }
+        else
+        {
+            return 0
+        }
+    }
 }
